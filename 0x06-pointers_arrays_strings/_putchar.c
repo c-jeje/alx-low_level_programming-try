61fc1b43354ac37cfc6671070a342481d307148e
Chabang28 <chabangmnisi@gmail.com>
2023-07-03 15:49:59 -0700

    m

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -10,6 +10,4 @@
  */                                                                                                                                     
 int _putchar(char c)                                                                                                                    
 {                                                                                                                                       
-        return (write(1, &c, 1));                                                                                                       
-}
-
+        return (write(1, &c, 1));                                        }
